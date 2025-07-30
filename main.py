d2bc61b854bc42ea68ef170cc95b59ae6bef6921
Peter Philip Johnson <125555434+maxprodigy@users.noreply.github.com>
2025-07-30 10:35:07 +0200

    Top G

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from environment.grid_env import CreativeGridEnv  # or your actual class
+from environment.grid_env import CreativeGridEnv  
 from environment.rendering import visualize_grid_env
 from stable_baselines3 import A2C, PPO  # or DQN, etc.
 
 if __name__ == "__main__":
     env = CreativeGridEnv()
-    model = A2C.load("models/pg/a2c_grid.zip") 
+    model = A2C.load("models/pg/a2c_grid.zip") # Make Changes Based On The Model Path Here
     visualize_grid_env(env, episodes=3, fps=5, tile_size=100, model=model)
